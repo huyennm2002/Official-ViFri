0f2e581bf5088f136839cb4e694ca3f3c1815448
Thang <leminhthang.tnl@gmail.com>
2023-06-01 22:30:55 -0500

    Update DimissKeyBoardView to tsx

--- client/src/components/DismissKeyboardView.tsx
+++ client/src/components/DismissKeyboardView.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { TouchableWithoutFeedback, Keyboard, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const DismissKeyboardHOC = (Comp) => {
-  return ({ children, ...props }) => (
+const DismissKeyboardHOC = (Comp: any) => {
+  return ({ children, ...props }:{ children: any, [key: string]: any }) => (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <Comp {...props}>
         {children}
