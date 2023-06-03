74a21b1b0fb69b09efaba9ce5636a005eb972375
Thang <leminhthang.tnl@gmail.com>
2023-06-03 11:13:47 -0500

    Download react native paper

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { PaperProvider } from 'react-native-paper';
 import AppNavigation from './src/navigation/index';
 
 function App() {
   return (
     <NavigationContainer>
-      <AppNavigation/>
+      <PaperProvider>
+        <AppNavigation/>
+      </PaperProvider>
     </NavigationContainer>
   );
 }
--- client/src/screens/FridgeItemListScreen.tsx
+++ client/src/screens/FridgeItemListScreen.tsx
@@ -1,10 +1,13 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export default function FridgeItemListScreen() {
   return (
     <SafeAreaView>
-      <Text>FridgeItemListScreen</Text>
+      <ScrollView>
+        
+      </ScrollView>
     </SafeAreaView>
   )
 }
