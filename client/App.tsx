7f02dc1395f62bbba1ca34d74073e17f71841d5f
Thang <leminhthang.tnl@gmail.com>
2023-06-03 15:42:22 -0500

    Add FridgeItemList screen

--- client/App.tsx
+++ client/App.tsx
@@ -1,15 +1,12 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { PaperProvider } from 'react-native-paper';
 import AppNavigation from './src/navigation/index';
 
 function App() {
   return (
     <NavigationContainer>
-      <PaperProvider>
         <AppNavigation/>
-      </PaperProvider>
     </NavigationContainer>
   );
 }
