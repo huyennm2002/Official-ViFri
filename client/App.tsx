2a67dc1e2f6fd3aae984abf27e5ded604d3d3385
Minh Huyen Nguyen <huyennm2002@gmail.com>
2023-06-03 12:05:19 -0500

    add redux

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,20 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { NavigationContainer } from '@react-navigation/native';
+import { store, persistor} from './src/redux/store';
 import AppNavigation from './src/navigation/index';
 
 function App() {
   return (
-    <NavigationContainer>
-      <AppNavigation/>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <AppNavigation/>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
 
