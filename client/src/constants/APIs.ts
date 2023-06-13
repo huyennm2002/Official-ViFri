eacbb4bfddcdd64b94d87b734cca2ffc72195a35
Minh Huyen Nguyen <huyennm2002@gmail.com>
2023-06-12 23:13:38 -0500

    add API

--- client/src/constants/APIs.ts
+++ client/src/constants/APIs.ts
@@ -1,4 +1,5 @@
-const HOSTNAME = '192.168.1.241'
+const HOSTNAME = '192.168.1.78'
 export const LOGIN_API = `http://${HOSTNAME}:3005/login`
 export const LOGOUT_API = `http://${HOSTNAME}:3005/logout`
 export const SIGNUP_API = `http://${HOSTNAME}:3005/signup`
+export const GET_USER_INFO_API = `http://${HOSTNAME}:3005/users`
