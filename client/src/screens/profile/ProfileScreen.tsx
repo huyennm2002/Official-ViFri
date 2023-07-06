98df967ad5c57fd23fb0f9b22cd1ded82c326300
Minh Huyen Nguyen <huyennm2002@gmail.com>
2023-07-06 15:37:56 -0500

    add navigation

--- client/src/screens/profile/ProfileScreen.tsx
+++ client/src/screens/profile/ProfileScreen.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { Text, StyleSheet, TextInput, Button, Image, View, Pressable } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import DismissKeyboardView from '../../components/DismissKeyboardView'
 import Header from '../../components/Header';
-import { USER_LOG_OUT } from '../../redux/action';
-import { RootState, store } from '../../redux/store';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import UserProfileHeader from '../../components/user/UserProfileHeader';
 import ProfileOptions from '../../components/user/ProfileOptions';
 
@@ -15,7 +9,7 @@ export default function ProfileScreen({navigation}) {
     <SafeAreaProvider>
       <Header/>
       <UserProfileHeader/>
-      <ProfileOptions/>
+      <ProfileOptions navigation={navigation}/>
     </SafeAreaProvider>
   )
 }
